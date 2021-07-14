388da7c37b9eaad7f19ff6ab49150012487baf7a
Roman Averkin <vaperoma@mail.ru>
2021-07-14 10:33:22 +0300

    bfbf

---
+++ Monkeys UIPicker/MonkeyTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MonkeyTableViewCell.swift
+//  Monkeys UIPicker
+//
+//  Created by Roman Averkin on 13.07.21.
+//
+
+import UIKit
+
+class MonkeyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var typeMonkey: UILabel!
+    
+    @IBOutlet weak var originMonkey: UILabel!
+}
